f616bb4e5f2303b2812a78b9b0bb71475a1db892
danielh13210 <danielh13210@gmail.com>
2026-01-18 06:55:24 +0800

    Put the list of levels in a central location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ import GridLayout from "./layouts/GridLayout";
 import Log from "./components/Log";
 import initialLampsRaw from "./data/test.json";
 import { useMemo, useState } from "react";
+import allLevels from "./components/LevelList"
 
 type Lamp = {
   uuid: string;     // we normalize keys (A1, A2...) into uuid
@@ -49,7 +50,7 @@ function normalizeToDict(data: any): LampsDict {
 
 function App() {
   const [lamps, setLamps] = useState<LampsDict>(() => normalizeToDict(initialLampsRaw));
-  const [level, setLevel] = useState<1 | 2>(1);
+  const [level, setLevel] = useState<typeof allLevels[number]>(1);
 
   const lampsArray = useMemo(() => Object.values(lamps), [lamps]);
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,35 +1,29 @@
+import allLevels from "./LevelList"
+
 type Props = {
-  level: 1 | 2;
-  setLevel: (lvl: 1 | 2) => void;
+  level: typeof allLevels[number];
+  setLevel: (lvl: typeof allLevels[number]) => void;
 };
 
 function Navbar({ level, setLevel }: Props) {
   return (
     <div className="w-full bg-[#2b2b2b] rounded-xl border border-white/5 px-6 py-4 flex items-center justify-center">
       <div className="bg-[#1f1f1f] border border-white/10 rounded-full p-1 flex gap-1">
-        <button
-          type="button"
-          onClick={() => setLevel(1)}
-          className={
-            level === 1
-              ? "px-4 py-2 rounded-full text-sm font-semibold bg-[#2d2d2d] text-yellow-300 shadow"
-              : "px-4 py-2 rounded-full text-sm font-semibold text-white/70 hover:text-white hover:bg-white/5 transition"
-          }
-        >
-          Level 1
-        </button>
-
-        <button
-          type="button"
-          onClick={() => setLevel(2)}
-          className={
-            level === 2
-              ? "px-4 py-2 rounded-full text-sm font-semibold bg-[#2d2d2d] text-yellow-300 shadow"
-              : "px-4 py-2 rounded-full text-sm font-semibold text-white/70 hover:text-white hover:bg-white/5 transition"
-          }
-        >
-          Level 2
-        </button>
+        {
+          allLevels.map(levelSelector => (
+            <button
+              type="button"
+              onClick={() => setLevel(levelSelector)}
+              className={
+                level === levelSelector
+                  ? "px-4 py-2 rounded-full text-sm font-semibold bg-[#2d2d2d] text-yellow-300 shadow"
+                  : "px-4 py-2 rounded-full text-sm font-semibold text-white/70 hover:text-white hover:bg-white/5 transition"
+              }
+            >
+              Level {levelSelector}
+            </button>
+          ))
+        }
       </div>
     </div>
   );
