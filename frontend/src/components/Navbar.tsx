5f5ba1edfed1171b816670bb9e4821462b0ae546
Lennon <lemoncode8888@gmail.com>
2026-01-18 07:18:52 +0800

    feat: added logo and renamed website name

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,8 +5,12 @@ type Props = {
 
 function Navbar({ level, setLevel }: Props) {
   return (
-    <div className="w-full bg-[#2b2b2b] rounded-xl border border-white/5 px-6 py-4 flex items-center justify-center">
-      <div className="bg-[#1f1f1f] border border-white/10 rounded-full p-1 flex gap-1">
+    <div className="w-full bg-[#2b2b2b] rounded-xl border border-white/5 px-6 py-4 flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <img src="/logo.png" alt="HushLight" className="h-8 w-8" />
+        <span className="font-semibold text-white">HushLight</span>
+      </div>
+      <div className="bg-[#1f1f1f] border border-white/10 justify-center rounded-full p-1 flex gap-1">
         <button
           type="button"
           onClick={() => setLevel(1)}
@@ -18,7 +22,6 @@ function Navbar({ level, setLevel }: Props) {
         >
           Level 1
         </button>
-
         <button
           type="button"
           onClick={() => setLevel(2)}
