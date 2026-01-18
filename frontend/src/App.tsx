5bc172fae1443a8c8998ff6a4d633d7ec8fe5dc2
danielh13210 <danielh13210@gmail.com>
2026-01-18 09:41:34 +0800

    Fix incorrect name setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -121,6 +121,7 @@ export default function App() {
       const nextStreak = isLoud ? prevStreak + 1 : 0;
       loudStreakRef.current[id] = nextStreak;
 
+      const name = lamp.label ?? lamp.uuid ?? id;
       if (!isLoud) {
         hasLoggedThisStreakEpisodeRef.current[id] = false;
         hasLoggedThisChangeEpisodeRef.current[id] = false;
@@ -140,7 +141,8 @@ export default function App() {
       const alreadyLoggedStreak = hasLoggedThisStreakEpisodeRef.current[id] ?? false;
       const alreadyLoggedChange = hasLoggedThisChangeEpisodeRef.current[id] ?? false;
       if(!alreadyLoggedChange){
-		newLogs.push({
+	
+        newLogs.push({
           ts: nowTs(),
           lamp: name,
           msg: `Became LOUD (≥ ${LOUD_THRESHOLD}%) Current: ${v}%`,
@@ -149,7 +151,6 @@ export default function App() {
         hasLoggedThisChangeEpisodeRef.current[id] = true;
       } else if (!alreadyLoggedStreak && nextStreak >= LOUD_STREAK_TO_LOG) {
         hasLoggedThisStreakEpisodeRef.current[id] = true;
-        const name = lamp.label ?? lamp.uuid ?? id;
 
         newLogs.push({
           ts: nowTs(),
